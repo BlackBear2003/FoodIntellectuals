b10dbe641d0e5a34be68bdd5be0fb6450110d72d
smmdtgxx <107744473+smmdtgxx@users.noreply.github.com>
2023-11-08 00:43:21 +0800

    给实体类的主键加上声明其主键策略的注解 (#8)
    * 添加注解

--- FoodIntellectuals-common/src/main/java/host/luke/FoodIntellectuals/common/entity/Review.java
+++ FoodIntellectuals-common/src/main/java/host/luke/FoodIntellectuals/common/entity/Review.java
@@ -1,6 +1,8 @@
 package host.luke.FoodIntellectuals.common.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -8,6 +10,7 @@ import javax.persistence.Table;
 @Table(name = "Review")
 public class Review {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Long storeId;
     private Long foodId;
