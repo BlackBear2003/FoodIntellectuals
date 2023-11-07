8f8bea581b2b7df86d3ee42532a64a5495a71455
smmdtgxx <107744473+smmdtgxx@users.noreply.github.com>
2023-11-07 23:44:08 +0800

    Update Canteen.java

--- Canteen.java
+++ Canteen.java
@@ -1,6 +1,8 @@
 package host.luke.FoodIntellectuals.common.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
