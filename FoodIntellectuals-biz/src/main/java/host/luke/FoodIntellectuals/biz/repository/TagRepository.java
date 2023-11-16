8bd3e8bc690642ea4af165f361b2ede87c67a3dd
SingAurora <2414786389@qq.com>
2023-11-16 22:08:13 +0800

    新增了biz模块下ReviewScoreController的实现 (#22)
    * 新增了biz模块下ReviewScoreController的实现

--- FoodIntellectuals-biz/src/main/java/host/luke/FoodIntellectuals/biz/repository/TagRepository.java
+++ FoodIntellectuals-biz/src/main/java/host/luke/FoodIntellectuals/biz/repository/TagRepository.java
@@ -1,8 +1,11 @@
 package host.luke.FoodIntellectuals.biz.repository;
 
 import host.luke.FoodIntellectuals.common.entity.Tag;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
   Tag findByTagTypeAndTagName(Integer tagType, String tagName);
+  List<Tag> findTagsByTagType(Integer tagType);
+  List<Tag> searchTagsByTagNameContainingIgnoreCase(String tagName);
 }
